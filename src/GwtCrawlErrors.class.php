e37bb1d51c44e550008cf0a393315434db4be3d5
arpit <rawat.arpit@gmail.com>
2014-03-24 12:51:31 +0000

    removed memory limit

--- src/GwtCrawlErrors.class.php
+++ src/GwtCrawlErrors.class.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('max_execution_time', 600);
-ini_set('memory_limit', '64M');
-
 
 /** GwtCrawlErrors
  *  ================================================================================
